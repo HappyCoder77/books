bc690b7eefb4a8be35d9049b4d62032b92f6a0a9
HappyCoder77 <saul_rr77@hotmail.com>
2026-03-24 15:56:26 -0400

    chapter-7

--- config/settings.py
+++ config/settings.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
     # third-party
     "crispy_forms",
     "crispy_bootstrap4",
+    "allauth",
+    "allauth.account",
     # local
     "accounts",
     "pages",
@@ -53,6 +56,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -139,10 +143,27 @@ STATICFILES_FINDERS = [
 
 AUTH_USER_MODEL = "accounts.CustomUser"
 
-LOGIN_REDIRECT_URL = "home"
-
-LOGOUT_REDIRECT_URL = "home"
 
 CRISPY_ALOWED_TEMPLATE_PACKS = "bootstrap4"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+# Django-allauth config
+LOGIN_REDIRECT_URL = "home"
+
+LOGOUT_REDIRECT_URL = "home"
+
+ACCOUNT_LOGOUT_REDIRECT = "home"
+
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+)
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+ACCOUNT_SESSION_REMEMBER = True
+
+ACCOUNT_SIGNUP_FIELDS = ["email", "username*", "password1*"]
